10586938cc71f78ba7a7b8444335d785aac00609
ColorFilter <johan@romantech.net>
2026-09-20 01:30:21 +0900

    chore(deps): update project dependencies (#94)

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
